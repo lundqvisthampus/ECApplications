f593c678e2fc27254c9185d0cf036aeee330570b
lundqvisthampus <lundqvisthampus@outlook.com>
2023-12-06 15:42:40 +0100

    Contact class and interface added

---
+++ ECConsoleApp/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using ECConsoleApp.Interfaces;
+
+namespace ECConsoleApp.Models;
+
+internal class Contact : IContact
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int PhoneNumber { get; set; }
+    public string Address { get; set; } = null!;
+}
